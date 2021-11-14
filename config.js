662d330566a0f84dee57d79eae8629b8b3687b8e
susako2003 <94290036+susako2003@users.noreply.github.com>
2021-11-14 17:52:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "YAHHH",
+    introTitle: 'Chị Quỳnh ',
+    introDesc: `Bình minh chưa tàn nắng đã lên
+    Em chưa sẵn sàng chị đã đến 
+    Biển chỉ đẹp khi hoàng hôn buông xuống
+    Tình chỉ đẹp khi em và chị yêu nhau :3`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Đó có phải là yêu hay không tại sao mà em lại tin hoài 🥰',
+    desc: 'Đó có phải là yêu hay không tại sao làm tim em ngây dại  ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Chị làm người yêu em nha <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Yahh 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'khi nào chị rảnh thì em đón chị đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100010800544331' 
 }
